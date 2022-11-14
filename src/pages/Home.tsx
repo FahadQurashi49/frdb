eb4f22c942d59b3c3a8d021025e32ef2873da07b
fahad.qureshi <fahad.qureshi@colwiz.com>
2022-11-15 00:07:28 +0500

    feat(Home): add area autocomplete box

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
-import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { Autocomplete, Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
 import { Box } from '@mui/system';
 import { useState } from 'react';
+
+import cityArea from '../resource/area.json';
 
 const cities = [
     'Karachi',
@@ -11,12 +13,25 @@ const cities = [
 ]
 
 function Home() {
+
     const [city, setCity] = useState('');
+    const cityAreaMap = new Map(cityArea as []);
+    const [areas, setAreas] = useState([] as string[]);
+    const [area, setArea] = useState('' as (string | null));
+
+    
 
     const handleCityChange = (e: SelectChangeEvent) => {
-        setCity(e.target.value);
+        const selectedCity: string = e.target.value;
+        const currentAreas = cityAreaMap.get(selectedCity) as string[];
+        setAreas(currentAreas);
+        setArea(null);
+        setCity(selectedCity);
     };
 
+    const handleAreaChange = (e: any, newValue: string | null) => {
+        setArea(newValue);
+    }
     return (
         <Box display="flex"
             justifyContent="center"
@@ -24,20 +39,34 @@ function Home() {
             minHeight="100vh"
         >
             <form>
-                <FormControl fullWidth>
-                    <InputLabel id="city-select-label">City</InputLabel>
-                    <Select
-                        labelId="city-select-label"
-                        id="city-select"
-                        label="City"
-                        autoWidth
-                        sx={{ px: '12px' }}
-                        value={city}
-                        onChange={handleCityChange}
-                    >
-                        {cities.map(city => <MenuItem value={city}>{city}</MenuItem>)}
-                    </Select>
-                </FormControl>
+                <Box display='flex' >
+                    <FormControl>
+                        <InputLabel id="city-select-label">City</InputLabel>
+                        <Select
+                            labelId="city-select-label"
+                            id="city-select"
+                            label="City"
+                            autoWidth
+                            value={city}
+                            sx={{ width: 150 }}
+                            onChange={handleCityChange}
+                        >
+                            {cities.map(city => <MenuItem key={city} value={city}>{city}</MenuItem>)}
+                        </Select>
+                    </FormControl>
+                    <Autocomplete
+                        disablePortal
+                        id="area-combo-box"
+                        disabled={!city}
+                        options={areas}
+                        value={area}
+                        onChange={handleAreaChange}
+                        sx={{ width: 200 }}
+                        renderInput={(params) => <TextField {...params} label="Area" />}
+                    />
+                    <Button variant='contained'>SUBMIT</Button>
+                </Box>
+                
             </form>
         </Box>
     );
