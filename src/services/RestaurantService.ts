fe1d49d9e3ababb4fdf53120712221fffec72f66
fahad.qureshi <fahad.qureshi@colwiz.com>
2022-12-07 22:43:57 +0500

    fix(Restaurant): fix restaurant page error handling

--- src/services/RestaurantService.ts
+++ src/services/RestaurantService.ts
@@ -5,22 +5,18 @@ class RestaurantService {
     private static restaurantEndpoint = `${ApiEndpoint}/restaurant`;
 
     async fetchRestaurants(cityArea?: string): Promise<Restaurant[] | null> {
-        try {
-            if (cityArea) {
-                const city = cityArea?.split('_').shift();
-                const area = cityArea?.split('_').pop();
-                const queryParam = encodeURIComponent(`#${city} #${area}`);
-                const response = await fetch(`${RestaurantService.restaurantEndpoint}?location=${queryParam}`);
-                const data = await response.json();
-                if (data?.status === 200) {
-                    return data.items.map(mapToRestaurant);
-                }
+        if (cityArea) {
+            const city = cityArea?.split('_').shift();
+            const area = cityArea?.split('_').pop();
+            const queryParam = encodeURIComponent(`#${city} #${area}`);
+            const response = await fetch(`${RestaurantService.restaurantEndpoint}?location=${queryParam}`);
+            const data = await response.json();
+            if (data?.status === 200) {
+                return data.items.map(mapToRestaurant);
             }
-            return null;
-        } catch (e) {
-            console.error(e);
-            return null;
+            throw Error(`Restaurants not fetched, status: ${data.status}`);
         }
+        throw Error('Please provide city and area');
     }
 }
 const restaurantService = new RestaurantService();
