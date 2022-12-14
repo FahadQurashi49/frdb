28c29f9be61ad698a13d2c7d429c8aa5f982dc60
fahad.qureshi <fahad.qureshi@colwiz.com>
2022-12-14 23:14:08 +0500

    refactor(Add Review): use form from react router in add review

--- src/pages/AddReview.tsx
+++ src/pages/AddReview.tsx
@@ -1,14 +1,17 @@
 import { PhotoCamera, Send } from "@mui/icons-material";
 import { Button, Rating, TextField, Typography } from "@mui/material";
 import { Box, Stack } from "@mui/system";
-import { useParams } from "react-router-dom";
+import { useParams, Form } from "react-router-dom";
 
-function AddReview() {
+export async function  action(data: any) {
+    const formData = await data.request.formData();
+    const reviewData = Object.fromEntries(formData);
+    console.log('hello world I am running!');
+    console.log('data: ', reviewData);
+}
+
+export function AddReview() {
     const { restaurantId } = useParams();
-
-    const handleSubmit = () => {
-
-    };
 
     return (
         <Box sx={{ mx: 5 }} mx = {5}>
@@ -16,10 +19,10 @@ function AddReview() {
                 sx={{ typography: { md: 'h4', sm: 'h5', xs: 'h6' } }} >
                 Share your expeirence at BBQ and Grill
             </Typography>
-            <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <Form method="post">
                 <Box mx={40} mt={5}>
                     <Stack spacing={2} justifyContent='start' alignItems='start'>
-                        <TextField multiline fullWidth rows={10}
+                        <TextField name="review" multiline fullWidth rows={10}
                             placeholder='Tell us about food, service, ambience and others' />
                         <Box>
                             <Rating name='review-rating' size='large' precision={0.5} />
@@ -28,12 +31,10 @@ function AddReview() {
                             Upload
                             <input hidden accept='image/*' multiple type='file' />
                         </Button>
-                        <Button disableElevation variant='contained' startIcon={<Send />} >Submit</Button>
+                        <Button type='submit' disableElevation variant='contained' startIcon={<Send />} >Submit</Button>
                     </Stack>
                 </Box>
-            </form>
+            </Form>
         </Box>
     );
 }
-
-export default AddReview;
